db292b34cf5d03f5e69b79d5c075447d87468275
Philipp Dikmann <mail@philippdikmann.de>
2016-05-26 19:18:51 +0200

    render and position improved

--- main.lisp
+++ main.lisp
@@ -1,20 +1,63 @@
+;; -----------------------------------------------------------------------------
+;; models
 (defparameter *nodes* '())
 (defparameter *edges* '())
 (defparameter *cells* '())
 
+(defstruct node
+  (name "NONAME")
+  (label "NOLABEL")
+  (shape "circle")
+  (style nil)
+  (pos nil)
+  (special nil))
+
 (defstruct edge
-  n1 n2 style)
+  (n1 0)
+  (n2 0)
+  (style nil))
 
-(defun add-node (label)
-  (setf *nodes* (reverse (cons label 
-                               (reverse *nodes*))))
-  *nodes*)
+(defun default-node (label)
+  (make-node :name (string (gensym))
+             :label label
+             :shape "circle"))
 
-(defun add-edge (n1 n2)
-  (setf *edges* (cons (list n1 n2) *edges*))
-  *edges*)
+(defun plain-node (label)
+  (make-node :name (string (gensym))
+             :label label
+             :shape "plaintext"))
 
 ;; -----------------------------------------------------------------------------
+;; making nodes and edges
+(defun add-node (node)
+  "returns index of node in *nodes*"
+  (setf *nodes* (reverse (cons node (reverse *nodes*))))
+  (1- (length *nodes*)))
+
+(defun add-edge (n1 n2 &optional (style nil))
+  (setf *edges* (cons (make-edge :n1 n1
+                                 :n2 n2
+                                 :style style)
+                      *edges*))
+  *edges*)
+
+(defun add-cell (label connections)
+  (let ((ci (add-node (plain-node label))))
+    (mapcar #'(lambda (ni) (add-edge ci ni "invis"))
+            connections)))
+
+(defun pin-node (label position)
+  "expects position as '(x y)"
+  (let ((n (first (member label *nodes* :key #'node-label :test #'equal))))
+    (setf (node-pos n) position)))
+
+(defun pin-last-node (position)
+  "expects position as '(x y)"
+  (let ((n (first (last *nodes*))))
+    (setf (node-pos n) position)))
+
+;; -----------------------------------------------------------------------------
+;; parse into neato format
 ;; helper
 (defun envelop (front back content)
   (concatenate 'string front content back))
@@ -30,20 +73,22 @@
 (defun graph (content)
   (envelop "graph G {" "}" content))
 
-(defun edge (lst)
-  (let ((n1n (string (nth (car lst) *nodes*)))
-        (n2n (string (nth (cadr lst) *nodes*))))
-    (concatenate 'string n1n " -- " n2n ";")))
+(defun edge (e)
+  (let ((n1n (node-name (nth (edge-n1 e) *nodes*)))
+        (n2n (node-name (nth (edge-n2 e) *nodes*))))
+    (if (edge-style e)
+        (concatenate 'string n1n " -- " n2n "[style=" (edge-style e) "];")
+        (concatenate 'string n1n " -- " n2n ";"))))
 
 (defun node (n)
-  (concatenate 'string (string n) "[shape=circle];"))
-
-;; (defun cell (lst)
-;;   (let ((c (string (car lst))))
-;;     (node c)
-;;     ))
-
-;; "{ edge[ style=invis ]; ... }"
+  (concatenate 'string (string (node-name n))
+               "[label=" (node-label n) ","
+               (when (node-pos n)
+                 (concatenate 'string "pos=\""
+                              (numstring (first (node-pos n))) ","
+                              (numstring (second (node-pos n)))
+                              "\","))
+               "shape=" (node-shape n) "];"))
 
 (defun render-nodes ()
   (graph (concatenate 'string
@@ -51,34 +96,35 @@
                       (collect (mapcar #'edge *edges*)))))
 
 ;; -----------------------------------------------------------------------------
-;; (add-node 'jim)
-;; (add-node 'bimbo)
-;; (add-node 'wim)
-;; (add-edge 0 1)
-;; (add-edge 0 2)
-
+;; construction
 (labels ((grid (x y &optional (cx 1) (cy 1))
            (let ((index (+ (1- cx)
                            (* x (1- cy)))))
              (cond ((and (eq cx x)
                          (eq cy y))
                     ;; finish grid
-                    (add-node (numstring index)))
+                    (add-node (default-node (numstring index)))
+                    (pin-last-node (list cx cy)))
                    ((eq cx x)
                     ;; finish row
-                    (add-node (numstring index))
+                    (add-node (default-node (numstring index)))
+                    (pin-last-node (list cx cy))
                     (unless (eq cy y) ; unless last row: connect rows
                       (add-edge index (+ x index)))
                     (grid x y 1 (1+ cy)))
                    (t
                     ;; add to current row
-                    (add-node (numstring index))
+                    (add-node (default-node (numstring index)))
+                    (pin-last-node (list cx cy))
                     (add-edge index (1+ index))
                     (unless (eq cy y) ; unless last row: connect rows
                       (add-edge index (+ x index)))
                     (grid x y (1+ cx) cy))))))
-  (grid 5 5))
+  (grid 3 3))
 
+(add-cell "doc" '(0 1 3 4))
+(add-cell "cool" '(1 2 4 5))
 
 (with-input-from-string (s (render-nodes))
-  (sb-ext:run-program "/usr/bin/neato" (list "-T" "png" "-o" "stream.png") :input s))
+  (sb-ext:run-program "/usr/bin/neato" (list "-T" "png" "-o" "stream.png") :input s)
+  s)
